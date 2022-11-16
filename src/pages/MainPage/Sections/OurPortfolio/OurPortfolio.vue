1a22b3a70234b267a0c0e040ad58d50df2b191df
Evgenii-K <inso.jk@gmail.com>
2022-11-16 09:55:09 +0700

    main-page/ portfolio section init

--- src/pages/MainPage/Sections/OurPortfolio/OurPortfolio.vue
+++ src/pages/MainPage/Sections/OurPortfolio/OurPortfolio.vue
@@ -5,9 +5,36 @@
     <div class="portfolio__article">
       all projects that we have already done , proven can help to use more comfortable, then can used by client from our business
     </div>
+    <div class="portfolio__works">
+      <div
+        v-for="work in works"
+        :key="work.text"
+        class="portfolio__work"
+        :style="work.background"
+      >
+        {{ work.text }}
+      </div>
+    </div>
+    <base-button
+      :click-handler="clickHandler"
+      outline
+      text="See All Portfolio"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import './style.scss'
+import BaseButton from 'src/components/ui/baseButton/BaseButton.vue'
+
+const works = [
+  { text: 'Design Furniture App', background: { backgroundImage: "url('../../../../assets/img/MainPage/work_cloud.png')"}},
+  { text: 'Cloud App', background: '../../../../assets/img/MainPage/work_cloud.png'},
+  { text: 'Design Byte App', background: '../../../../assets/img/MainPage/work_byte.png'},
+
+]
+
+const clickHandler = () => {
+  console.log('Portfolio');
+}
 </script>
