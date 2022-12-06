e6ad1479cce00ff64447f6487005470a9301eac7
Evgenii-K <inso.jk@gmail.com>
2022-12-06 23:07:23 +0700

    feature/adaptive collaboration, footer

--- src/pages/MainPage/Sections/Collaboration/Collaboration.vue
+++ src/pages/MainPage/Sections/Collaboration/Collaboration.vue
@@ -5,24 +5,42 @@
         <div class="photo-block__image" />
         <div class="photo-block__image" />
       </div>
+      <dots
+        class="photo-block__dots photo-block__dots--blue"
+        :color="dotColor"
+        :size="10"
+      />
+      <dots
+        class="photo-block__dots photo-block__dots--red-first"
+        color="#FF5C00"
+      />
+      <dots
+        class="photo-block__dots photo-block__dots--red-second"
+        color="#FF5C00"
+      />
     </div>
-    <h2 class="collaboration__header">Interesting Collaboration With Us?</h2>
-    <div class="collaboration__content">
-      Help you to reach your business goal
+    <div class="collaboration__content-block">
+      <h2 class="collaboration__header">Interesting Collaboration With Us?</h2>
+      <div class="collaboration__content">
+        Help you to reach your business goal
+      </div>
+      <base-button
+        :click-handler="clickHandler"
+        text="Get Started"
+      />
     </div>
-    <base-button
-      :click-handler="clickHandler"
-      text="Get Started"
-    />
   </section>
 </template>
 
 <script setup lang="ts">
   import './style.scss'
   import BaseButton from 'src/components/ui/baseButton/BaseButton.vue'
-  // import Dots from 'src/components/ui/dots/Dots.vue'
+  import Dots from 'src/components/ui/dots/Dots.vue'
   import { useStore } from 'src/store'
-  import { computed } from '@vue/reactivity'
+  import { computed, ref } from '@vue/reactivity'
+  import { onBeforeMount } from 'vue'
+
+  const dotColor = ref('#377DFF')
 
   const state = useStore()
 
@@ -34,4 +52,10 @@
   const clickHandler = () => {
     state.commit('general/mutateIsSendOpen', !isSendOpen.value)
   }
+
+  onBeforeMount(() => {
+    if (document.body.dataset.theme === 'dark') {
+      dotColor.value = '#E2E2E2'
+    }
+  })
 </script>
