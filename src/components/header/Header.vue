bc1e85b69bf86bf7790c37f0e535a2bcfed3d9dc
Evgenii K <inso.jk@gmail.com>
2023-03-15 15:19:23 +0700

    feature/loader subtitle fix, header styles

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,42 +1,43 @@
 <template>
-  <div :class="['header', { 'header--hidden': isHidden }]">
+  <div :class="['header', 'q-page-container', { 'header--hidden': isHidden }]">
     <div class="header__wrapper">
       <div class="header__label">
         <q-avatar class="header__logo">
           <img src="../../assets/img/header_logo.png" />
         </q-avatar>
-        <div class="header__name">
-          Agency<span>Creative</span>
-        </div>
+        <div class="header__name">Agency<span>Creative</span></div>
       </div>
       <div class="header__nav">
         <nav-block />
       </div>
-      <burger-button class="header__button" @click="clickHandler" />
+      <burger-button
+        class="header__button"
+        @click="clickHandler"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue'
-import './style.scss'
-import BurgerButton from '../ui/burgerButton/BurgerButton.vue';
-import NavBlock from '../navBlock/NavBlock.vue';
-import { useStore } from 'src/store'
-import { computed } from '@vue/reactivity'
+  import { defineProps } from 'vue'
+  import './style.scss'
+  import BurgerButton from '../ui/burgerButton/BurgerButton.vue'
+  import NavBlock from '../navBlock/NavBlock.vue'
+  import { useStore } from 'src/store'
+  import { computed } from '@vue/reactivity'
 
-defineProps({
-  isHidden: { type: Boolean, default: false },
-})
+  defineProps({
+    isHidden: { type: Boolean, default: false },
+  })
 
-const state = useStore()
+  const state = useStore()
 
-const isMenuOpen = computed(() => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  return (state.getters['general/getIsMenuOpen'] as boolean)
-})
+  const isMenuOpen = computed(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    return state.getters['general/getIsMenuOpen'] as boolean
+  })
 
-const clickHandler = () => {
-  state.commit('general/mutateIsMenuOpen', !isMenuOpen.value)
-}
+  const clickHandler = () => {
+    state.commit('general/mutateIsMenuOpen', !isMenuOpen.value)
+  }
 </script>
--- src/pages/MainPage/Sections/Testimonial/Testimonial.vue
+++ src/pages/MainPage/Sections/Testimonial/Testimonial.vue
@@ -2,37 +2,44 @@
   <section class="testimonial__wrapper">
     <h3 class="testimonial__subtitle">Testimonial</h3>
     <h2 class="testimonial__title">People Talk about us</h2>
-    <custom-slider :slide-items="feedbacks" :break-points="breakPoints" />
+    <custom-slider
+      :slide-items="feedbacks"
+      :break-points="breakPoints"
+    />
   </section>
 </template>
 
 <script setup lang="ts">
-import './style.scss'
-import { computed } from 'vue'
-import { useStore } from 'src/store'
-import { useI18n } from 'vue-i18n'
-import { IReviews } from 'src/components/models'
-import CustomSlider from 'src/components/CustomSlider/CustomSlider.vue'
+  import './style.scss'
+  import { computed } from 'vue'
+  import { useStore } from 'src/store'
+  import { useI18n } from 'vue-i18n'
+  import { IReviews } from 'src/components/models'
+  import CustomSlider from 'src/components/CustomSlider/CustomSlider.vue'
 
-const { locale } = useI18n({ useScope: 'global' })
+  const { locale } = useI18n({ useScope: 'global' })
 
-const state = useStore()
+  const state = useStore()
 
-const breakPoints = [
-  { maxWidth: 768, count: 1, gap: 25 },
-  { maxWidth: 1280, count: 2, gap: 30 },
-]
+  const breakPoints = [
+    { maxWidth: 768, count: 1, gap: 25 },
+    { maxWidth: 1280, count: 2, gap: 30 },
+  ]
 
-const feedbacks = computed(() => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const reviews = (state.getters['general/getReviews'] as IReviews[])
+  const feedbacks = computed(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const reviews = state.getters['general/getReviews'] as IReviews[]
 
-  return reviews.map((review, index) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const newItem = { id: review.id, avatar: require('src/assets/img/MainPage/avatar.png'), index: index + 1 }
-    if (locale.value === 'ru') return { ...newItem, ...review.ru }
+    return reviews.map((review, index) => {
+      const newItem = {
+        id: review.id,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        avatar: require('src/assets/img/MainPage/avatar.png'),
+        index: index + 1,
+      }
+      if (locale.value === 'ru') return { ...newItem, ...review.ru }
 
-    return { ...newItem, ...review.en }
+      return { ...newItem, ...review.en }
+    })
   })
-})
 </script>
